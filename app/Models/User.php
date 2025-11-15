7997b37e7dd30b58a603cac0d7d352e2a173c009
FaithMuthoni959 <famberry2000@gamil.com>
2025-11-16 00:55:49 +0300

    deleted some models and added new ones. Also made the relationships

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,47 +2,59 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
-    /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var list<string>
-     */
     protected $fillable = [
         'name',
         'email',
         'password',
+        'avatar_url',
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var list<string>
-     */
     protected $hidden = [
         'password',
         'remember_token',
     ];
 
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
     protected function casts(): array
     {
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    // Relationships
+    public function courses()
+    {
+        return $this->hasMany(Course::class);
+    }
+
+    public function tasks()
+    {
+        return $this->hasMany(Task::class);
+    }
+
+    public function studyGroups()
+    {
+        return $this->belongsToMany(StudyGroup::class, 'group_members')
+                    ->withPivot('role')
+                    ->withTimestamps();
+    }
+
+    public function createdStudyGroups()
+    {
+        return $this->hasMany(StudyGroup::class, 'created_by');
+    }
+
+    public function reminders()
+    {
+        return $this->hasMany(Reminder::class);
     }
 }
