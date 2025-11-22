6f8fd90c6d0fe7ea4c54bd2a3a02af67c1a8d8fa
Ndubito <acharnathan1@gmail.com>
2025-11-22 23:33:06 +0300

    adding routes to sidebar

--- resources/views/layouts/partials/sidebar.blade.php
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -4,17 +4,18 @@
         <li class="{{ request()->routeIs('dashboard') ? 'active' : '' }}">
             <a href="{{ route('dashboard') }}">Home</a>
         </li>
-        <li><a href="#">Add new task</a></li>
-        <li><a href="#">Search</a></li>
-        <li><a href="#">Notes</a></li>
-        <li><a href="#">Calendar</a></li>
-        <li><a href="#">Reminders</a></li>
-        <li><a href="#">My Projects</a></li>
-        <li><a href="#">Inbox</a></li>
-        <li><a href="#">Upcoming</a></li>
-        <li><a href="#">Archive</a></li>
-        <li><a href="#">Settings</a></li>
+        <li><a href="{{ route('tasks.create') }}">Add new task</a></li>
+        <li><a href="{{ route('search') }}">Search</a></li>
+        <li><a href="{{ route('notes') }}">Notes</a></li>
+        <li><a href="{{ route('calendar') }}">Calendar</a></li>
+        <li><a href="{{ route('reminders') }}">Reminders</a></li>
+        <li><a href="{{ route('projects') }}">My Projects</a></li>
+        <li><a href="{{ route('inbox') }}">Inbox</a></li>
+        <li><a href="{{ route('upcoming') }}">Upcoming</a></li>
+        <li><a href="{{ route('archive') }}">Archive</a></li>
+        <li><a href="{{ route('settings') }}">Settings</a></li>
         <li class="{{ request()->routeIs('courses') ? 'active' : '' }}">
+            <a href="{{ route('courses') }}">Courses</a>
         </li>
     </ul>
 </aside>
